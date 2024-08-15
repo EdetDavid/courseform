cccbb19d16d2f58553cec47084ab35c26b7bf0d1
Dvooskid <dvooskid1234@gmail.com>
2024-08-15 09:37:01 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
-# build_files.sh
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
 
-echo "Make Migrations..."
-python3.9 manage.py makemigrations --no-input
-python3.9 manage.py migrate --no-input
-
-echo "Collect Static Files..."
-python3.9 manage.py collectstatic --no-input --clear
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
